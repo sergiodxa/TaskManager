5f93f975c9d0f283bb066012ab5ae80f61e67222
Sergio Daniel Xalambrí <sergio@xalambri.com.ar>
2014-06-16 23:15:40 -0300

    controlador de projectos terminado

---
+++ controllers/project.js
@@ -0,0 +1,52 @@
+var model = require('../models/project');
+
+exports.getAll = function (req, res) {
+  model.getAll(function (projects) {
+    res.send(projects);
+  });
+};
+
+exports.getSingle = function (req, res) {
+  var id = req.params.id;
+
+  model.getSingle(id, function (project) {
+    res.send(project);
+  });
+};
+
+exports.add = function (req, res) {
+  var project = JSON.parse(req.query.project);
+
+  model.add(project, function (response) {
+    if (response === 'Project created') {
+      res.send(response);
+    } else if (response === false) {
+      res.send('An error has ocurred');
+    }
+  });
+};
+
+exports.edit = function (req, res) {
+  var id = req.params.id;
+  var project = JSON.parse(req.query.project);
+
+  model.edit(id, project, function (response) {
+    if (response === 'Project edited') {
+      res.send(response);
+    } else if (response === false) {
+      res.send('An error has ocurred');
+    };
+  });
+};
+
+exports.erase = function (req, res) {
+  var id = req.params.id;
+
+  model.erase(id, function (response) {
+    if (response === 'Project deleted') {
+      res.send('Project deleted');
+    } else if (response === false) {
+      res.send('Error');
+    }
+  })
+};
