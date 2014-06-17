3ad503abb2142a0479efa8438d81acfd43c80adb
sergiodxa <sergio@xalambri.com.ar>
2014-06-17 15:51:55 -0300

    nueva tarea para unir todos los js minificados en un solo archivo final

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,12 @@
-var gulp   = require('gulp'),
+var gulp        = require('gulp'),
     runSequence = require('run-sequence'),
-    jasmine = require('gulp-jasmine'),
-    jsmin  = require('gulp-jsmin'),
-    rename = require('gulp-rename'),
-    watch  = require('gulp-watch');
+    jasmine     = require('gulp-jasmine'),
+    jsmin       = require('gulp-jsmin'),
+    rename      = require('gulp-rename'),
+    watch       = require('gulp-watch');
+    concat      = require('gulp-concat');
 
-gulp.task('build', function(callback) {
+gulp.task('minify', function(callback) {
   runSequence('minify controllers',
               'minify services',
               'minify app',
@@ -17,7 +18,7 @@ gulp.task('minify controllers', function () {
       .pipe(watch())
       .pipe(jsmin())
       .pipe(rename({suffix: '.min'}))
-      .pipe(gulp.dest('front/dist'));
+      .pipe(gulp.dest('front/minified'));
 });
 
 gulp.task('minify services', function () {
@@ -25,7 +26,7 @@ gulp.task('minify services', function () {
       .pipe(watch())
       .pipe(jsmin())
       .pipe(rename({suffix: '.min'}))
-      .pipe(gulp.dest('front/dist'));
+      .pipe(gulp.dest('front/minified'));
 });
 
 gulp.task('minify app', function () {
@@ -33,11 +34,26 @@ gulp.task('minify app', function () {
       .pipe(watch())
       .pipe(jsmin())
       .pipe(rename({suffix: '.min'}))
-      .pipe(gulp.dest('front/dist'));
+      .pipe(gulp.dest('front/minified'));
 });
 
 gulp.task('jasmine tests', function () {
     gulp.src('spec/*.spec.js')
         .pipe(watch())
         .pipe(jasmine());
+});
+
+gulp.task('merge', function () {
+  gulp.src(['./front/vendor/jquery/dist/jquery.min.js',
+           './front/vendor/bootstrap/dist/js/bootstrap.min.js',
+           './front/vendor/angular/angular.min.js',
+           './front/vendor/angular-route/angular-route.min.js',
+           './front/minified/app.min.js',
+           './front/minified/ClientService.min.js',
+           './front/minified/ProjectService.min.js',
+           './front/minified/TaskService.min.js',
+           './front/minified/UserService.min.js',
+           './front/minified/**/*.min.js'])
+      .pipe(concat('main.js'))
+      .pipe(gulp.dest('front/dist'));
 });
