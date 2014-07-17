66d681d91f99a45a44a3bf3edaf723643e2b8375
sergiodxa <sergio@xalambri.com.ar>
2014-07-17 18:00:45 -0300

    corregidos formularios para crear y editar proyectos

--- app/controllers/users/UserNewCtrl.js
+++ app/controllers/users/UserNewCtrl.js
@@ -1,17 +1,15 @@
 function UserNewCtrl ($scope, users, encryptor, session) {
   session.auth();
 
-
   $scope.newPassIncorrect = false;
   $scope.userCreated = false;
 
   $scope.user = {};
 
   $scope.newPassCheck = function () {
-    var newPass = encryptor.md5($scope.user.newPass);
-    if (newPass !== '' && $scope.user.repeatPass !== '' && (newPass === $scope.user.repeatPass)) {
+    if ($scope.user.newPass !== '' && $scope.user.repeatPass !== '' && ($scope.user.newPass !== $scope.user.repeatPass)) {
       $scope.newPassIncorrect = false;
-    } else if (newPass === '') {
+    } else if ($scope.user.newPass === '') {
       $scope.newPassIncorrect = false;
     } else {
       $scope.newPassIncorrect = true;
