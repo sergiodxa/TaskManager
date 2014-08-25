92c3161d2d20a3e32e99210303fed54f87ddb3ca
sergiodxa <sergio@xalambri.com.ar>
2014-08-25 17:12:08 -0300

    seed de proyectos funcionando

--- seed.js
+++ seed.js
@@ -1,10 +1,31 @@
 var mongoose = require('mongoose');
+var series   = require('run-series');
+
 mongoose.connect('mongodb://localhost/taskmanager');
 
 var SeedUsers    = require('./seeds/SeedUsers');
 var SeedClients  = require('./seeds/SeedClients');
 var SeedProjects = require('./seeds/SeedProjects');
 
-SeedUsers(10);
-SeedClients(5);
-SeedProjects();
+series([
+  function (callback) {
+    SeedClients(5);
+    callback(null);
+  },
+  function (callback) {
+    SeedUsers(10);
+    callback(null);
+  },
+  function (callback) {
+    setTimeout(function () {
+      SeedProjects();
+      callback(null);
+    }, 500);
+  }
+], function (err, res) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('Seed ready');
+  }
+});
--- seeds/SeedProjects.js
+++ seeds/SeedProjects.js
@@ -1,54 +1,67 @@
-var mongoose = require('mongoose');
-var Client   = require('../app/models/ModelClient');
-var Project  = require('../app/models/ModelProject');
-var User     = require('../app/models/ModelUser');
+var mongoose  = require('mongoose');
+var waterfall = require('run-waterfall');
+var Client    = require('../app/models/ModelClient');
+var Project   = require('../app/models/ModelProject');
+var User      = require('../app/models/ModelUser');
 
-var projectNames = ['WebApp', 'Corporative Website', 'Intranet', 'eCommerce', '', 'Blog', 'Mobile App'];
+var projectNames = ['WebApp', 'Corporative Website', 'Intranet', 'eCommerce', 'Blog', 'Mobile App'];
 
-function generateProjects () {
-  Client.find(function (err, clients) {
-    if (err) {
-      console.error(err);
-      return;
-    } else {
+function generateProjects() {
+  waterfall([
+    function (callback) {
+      Client.find(function (err, clients) {
+        if (err) {
+          callback(err);
+        } else {
+          callback(null, clients);
+        };
+      });
+    },
+    function (clients, callback) {
       User.find({ position: 'Project Leader' }, function (err, users) {
         if (err) {
-          console.error(err);
-          return;
+          callback(err);
         } else {
-          for (var i = 0; i < clients.length; i++) {
-            var projectName = projectNames[Math.floor(Math.random() * projectNames)];
-            var description = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi, magnam. Eius, qui dolorum repudiandae exercitationem expedita fuga sapiente suscipit. Commodi dolores asperiores nesciunt fugiat voluptates. Ex atque perferendis explicabo minima!';
-
-            var releaseDateYear  = 2015;
-            var releaseDateMonth = Math.floor((Math.random() * 12) + 1);
-            var releaseDateDay   = 1;
-
-            var owner         = clients[i]._id;
-            var projectLeader = users[Math.floor(Math.random() * users)]._id;
-
-            var instance = new Project({
-              projectName  : projectName,
-              description  : description,
-              releaseDate  : new Date(releaseDateYear, releaseDateMonth, releaseDateDay),
-              githubRepo   : null,
-              owner        : mongoose.Types.ObjectId(owner),
-              projectLeader: mongoose.Types.ObjectId(data.projectLeader)
-            });
-
-            instance.save(function (err, res) {
-              if (err) {
-                console.error(err);
-                return;
-              };
-            });
-          };
-        }
+          callback(null, clients, users);
+        };
       });
-    }
-  });
+    }, function (clients, users, callback) {
+      for (var i = 0; i < clients.length; i++) {
+        var projectName = projectNames[Math.floor(Math.random() * projectNames.length)];
+        var description = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi, magnam. Eius, qui dolorum repudiandae exercitationem expedita fuga sapiente suscipit. Commodi dolores asperiores nesciunt fugiat voluptates. Ex atque perferendis explicabo minima!';
 
-  console.log('Projects generated');
+        var releaseDateYear  = 2015;
+        var releaseDateMonth = Math.floor((Math.random() * 12) + 1);
+        var releaseDateDay   = 1;
+
+        var owner         = clients[i]._id;
+        var projectLeader = users[Math.floor(Math.random() * users.length)]._id;
+
+        var instance = new Project({
+          projectName  : projectName,
+          description  : description,
+          releaseDate  : new Date(releaseDateYear, releaseDateMonth, releaseDateDay),
+          githubRepo   : null,
+          owner        : mongoose.Types.ObjectId(owner),
+          projectLeader: mongoose.Types.ObjectId(projectLeader)
+        });
+
+        instance.save(function (err, res) {
+          if (err) {
+            callback(err);
+          } else {
+          };
+        });
+      };
+      callback(null);
+    }
+  ], function (err) {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log('Projects generated');
+    };
+  });
 };
 
 module.exports = generateProjects;
