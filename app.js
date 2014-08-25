195f9ccf224c43130ee653c0fdb139851dba208c
sergiodxa <sergio@xalambri.com.ar>
2014-08-25 17:43:56 -0300

    se implementó un cluster para tener múltiples hilos y se eliminaron archivos obsoletos

--- app.js
+++ app.js
@@ -1,51 +1,87 @@
-// Dependencies
-var express  = require('express');
-var http     = require('http');
-var io       = require('socket.io');
-var mongoose = require('mongoose');
+var cluster  = require('cluster');
 
-// Start app
-var app = express();
-server  = http.createServer(app)
+if (cluster.isMaster) {
+  // Count the machine's CPUs
+  var cpuCount = require('os').cpus().length;
 
-// App Config
-app.use(express.static(__dirname + '/public'));
+  // Create a worker for each CPU
+  for (var i = 0; i < cpuCount; i += 1) {
+    cluster.fork();
+  }
 
-var port = Number(process.env.PORT || 3000);
+  // Listen for dying workers
+  cluster.on('exit', function (worker) {
+    // Replace the dead worker, we're not sentimental
+    console.log('Worker ' + worker.id + ' died :(');
 
-server.listen(port, function () {
-  console.log('Running on ' + port);
-});
+    cluster.fork();
+  });
 
-// Mongoose connect
-mongoose.connect('mongodb://localhost/taskmanager');
+  // As workers come up.
+  cluster.on('listening', function(worker, address) {
+    console.log("A worker with #"+worker.id+" is now connected to " + address.address + ":" + address.port);
+  });
 
-// Controllers
-var CtrlClient  = require('./app/controllers/CtrlClient');
-var CtrlProject = require('./app/controllers/CtrlProject');
-var CtrlTask    = require('./app/controllers/CtrlTask');
-var CtrlUser    = require('./app/controllers/CtrlUser');
-var CtrlSession = require('./app/controllers/CtrlSession');
+  // Count requestes
+  function messageHandler(msg) {
+    if (msg.cmd && msg.cmd == 'notifyRequest') {
+      numReqs += 1;
+    }
+  }
 
-// App Routes
-app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/public/app.html');
-});
+  Object.keys(cluster.workers).forEach(function(id) {
+    cluster.workers[id].on('message', messageHandler);
+  });
+} else {
+  // Dependencies
+  var express  = require('express');
+  var http     = require('http');
+  var io       = require('socket.io');
+  var mongoose = require('mongoose');
 
-// Session routes
-app.get('/session/login', CtrlSession.login);
-app.post('/session/auth', CtrlSession.auth);
-app.post('/session/logout', CtrlSession.logout);
+  // Start app
+  var app = express();
+  server  = http.createServer(app)
 
-// Socket.io initialized
-io = io.listen(server);
+  // App Config
+  app.use(express.static(__dirname + '/public'));
 
-// Socket.io events
-io.on('connection', function (socket) {
-  CtrlClient.io(socket);
-  CtrlProject.io(socket);
-  CtrlTask.io(socket);
-  CtrlUser.io(socket);
-});
+  var port = Number(process.env.PORT || 3000);
 
-console.log('TaskManger started - App running in the port ' + port);
+  server.listen(port, function () {
+    console.log('Running on ' + port);
+  });
+
+  // Mongoose connect
+  mongoose.connect('mongodb://localhost/taskmanager');
+
+  // Controllers
+  var CtrlClient  = require('./app/controllers/CtrlClient');
+  var CtrlProject = require('./app/controllers/CtrlProject');
+  var CtrlTask    = require('./app/controllers/CtrlTask');
+  var CtrlUser    = require('./app/controllers/CtrlUser');
+  var CtrlSession = require('./app/controllers/CtrlSession');
+
+  // App Routes
+  app.get('/', function (req, res) {
+    res.sendfile(__dirname + '/public/app.html');
+  });
+
+  // Session routes
+  app.get('/session/login', CtrlSession.login);
+  app.post('/session/auth', CtrlSession.auth);
+  app.post('/session/logout', CtrlSession.logout);
+
+  // Socket.io initialized
+  io = io.listen(server);
+
+  // Socket.io events
+  io.on('connection', function (socket) {
+    CtrlClient.io(socket);
+    CtrlProject.io(socket);
+    CtrlTask.io(socket);
+    CtrlUser.io(socket);
+  });
+
+  console.log('TaskManger started - App running in the port ' + port);
+}
